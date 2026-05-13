4cb64ddbec236028ec2fa6130c145cf4822199a6
gryope <45483433+gryope@users.noreply.github.com>
2026-05-13 13:06:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Luova/', 
+})
 })
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
