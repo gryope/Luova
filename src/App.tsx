5b56b738bd077efca44b68b20244b79fd6ed7c33
gryope <45483433+gryope@users.noreply.github.com>
2026-05-15 13:41:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ const COMPANIES: Company[] = [
     description: "Aiven is your AI-ready Open Source Data Platform.",
     fullDescription: "Aiven is an AI-ready open source data platform company, helping organizations gain more value from their data. Aiven’s cloud platform combines open choice services to stream, store, and serve data, simply, securely, and rapidly across major cloud providers to power innovation. Aiven is trusted by thousands of customers to create next-generation applications confidently and quickly.Aiven is headquartered in Helsinki and has hubs in Amsterdam, Berlin, Paris, London, Singapore, Sydney, Auckland, Austin and Toronto.",
     image: "/Luova/images/companies/Data1.jpg",
+    mission: "To simplify cloud infrastructure for developers worldwide.",
     tags: ["Databases", "Software"],
     website: "aiven.io/"
   },
